a2282a3d26354abaee12132d4303b53ca5d1ea17
buckethead1986 <rspeckpfs@gmail.com>
2017-11-01 11:09:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, dayJoined) {
+    this.name = name;
+    this.startDate = new Date(dayJoined);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear() - 1
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const hor = Math.abs(parseInt(this.beginningLocation["vertical"]) - parseInt(this.endingLocation["vertical"]));
+    const vert = Math.abs(eastWest.indexOf(this.beginningLocation["horizontal"]) - eastWest.indexOf(this.endingLocation["horizontal"]));
+    return hor + vert;
+  }
+
+  estimatedTime(peakHours) {
+    // (peakHours) ? return this.blocksTravelled() / 2 : return this.blocksTravelled() / 3
+    if (peakHours) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
